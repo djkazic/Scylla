1a58882f8748ea7a037a707f6fa34bc2b81e1f92
Kevin Cai <kevincai96@gmail.com>
2016-09-17 12:55:59 -0400

    Implemented base schema of Peer

--- src/main/java/org/alopex/scylla/Peer.java
+++ src/main/java/org/alopex/scylla/Peer.java
@@ -1,4 +1,15 @@
 package org.alopex.scylla;
 
+import com.esotericsoftware.kryonet.Connection;
+
 public class Peer {
+
+	private Connection connection;
+	private String mutex;
+	private int direction;
+
+	public Peer(Connection connection, int direction) {
+		this.connection = connection;
+		this.direction = direction;
+	}
 }
