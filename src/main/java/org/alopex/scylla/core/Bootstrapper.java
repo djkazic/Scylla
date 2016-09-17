9d66d08a757ef0dad6674469b942409b62212b07
Kevin Cai <kevincai96@gmail.com>
2016-09-17 13:20:28 -0400

    Patched Utils log method for Bootstrapper

--- src/main/java/org/alopex/scylla/core/Bootstrapper.java
+++ src/main/java/org/alopex/scylla/core/Bootstrapper.java
@@ -2,6 +2,7 @@ package org.alopex.scylla.core;
 
 import org.alopex.scylla.net.p2p.Peer;
 import org.alopex.scylla.net.socks.SOCKSProxy;
+import org.alopex.scylla.utils.Utils;
 
 import java.util.ArrayList;
 
@@ -9,6 +10,8 @@ import java.util.ArrayList;
  * @author Kevin Cai on 9/17/2016.
  */
 public class Bootstrapper {
+
+	private static final String CLASS_NAME =  "Bootstrapper";
 
 	public static ArrayList<Peer> peers;
 	public static String selfMutex;
@@ -21,6 +24,7 @@ public class Bootstrapper {
 
 		// Bootstrapper code here
 		try {
+			Utils.log(CLASS_NAME, "Creating SOCKSProxy instance...", false);
 			socksProxy = new SOCKSProxy();
 		} catch (Exception ex) {
 			ex.printStackTrace();
