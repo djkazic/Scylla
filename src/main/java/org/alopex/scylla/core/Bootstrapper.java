af7d13d7ee32db5d6418fd231cb3cfd0f78d51c7
Kevin Cai <kevincai96@gmail.com>
2016-09-17 13:13:46 -0400

    Migrated main method call from SOCKSProxy to Bootstrapper

--- src/main/java/org/alopex/scylla/core/Bootstrapper.java
+++ src/main/java/org/alopex/scylla/core/Bootstrapper.java
@@ -1,6 +1,7 @@
 package org.alopex.scylla.core;
 
 import org.alopex.scylla.net.p2p.Peer;
+import org.alopex.scylla.net.socks.SOCKSProxy;
 
 import java.util.ArrayList;
 
@@ -12,10 +13,17 @@ public class Bootstrapper {
 	public static ArrayList<Peer> peers;
 	public static String selfMutex;
 
+	public static SOCKSProxy socksProxy;
+
 	public static void main(String[] args) {
 		// Instance var instantiation segment
 		peers = new ArrayList<Peer> ();
 
 		// Bootstrapper code here
+		try {
+			socksProxy = new SOCKSProxy();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
