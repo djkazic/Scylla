2c0864b365ce370ab20bf84aab0aa81cd5a569eb
Kevin Cai <kevincai96@gmail.com>
2016-09-17 12:57:07 -0400

    Added bootstrapper class

---
+++ src/main/java/org/alopex/scylla/core/Bootstrap.java
@@ -0,0 +1,11 @@
+package org.alopex.scylla.core;
+
+/**
+ * @author Kevin Cai on 9/17/2016.
+ */
+public class Bootstrap {
+
+	public static void main(String[] args) {
+		// Bootstrap code here
+	}
+}
